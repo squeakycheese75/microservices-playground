96b6fe648ee787eb4c0775cbae973926877b7eb9
Niall Grant <niall.grant@and.digital>
2020-05-01 16:41:17 +0100

    Add express to app2
    Adds express to app2. Configures docker-compose.yml and updates
    Dockerfile.

---
+++ app2/index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const amqp = require('amqplib/callback_api');
+const app = express()
+
+app.get('/hello', function (req, res) {
+  res.send('Hello World')
+})
+app.listen(5000)
+
+amqp.connect('amqp://rabbitmq', function (error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function (error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+
+    var queue = 'Hello';
+
+    channel.assertQueue(queue, {
+      durable: false,
+    });
+
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
+
+    channel.consume(
+      queue,
+      function (msg) {
+        console.log(' [x] Received %s', msg.content.toString());
+      },
+      {
+        noAck: true,
+      }
+    );
+  });
+});
+
+
