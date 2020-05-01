7428ea1b594ab7a1151ab1ef5f3dec669d77a5a9
Jamie Wooltorton <jamie.wooltorton@and.digital>
2020-05-01 13:23:22 +0100

    fixing up linting

--- app1/send.js
+++ app1/send.js
@@ -1,11 +1,11 @@
-var amqp = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 
 async function sendMessage(queue, msg) {
-  amqp.connect('amqp://localhost', function (error0, connection) {
+  amqp.connect('amqp://localhost', (error0, connection) => {
     if (error0) {
       throw error0;
     }
-    connection.createChannel(function (error1, channel) {
+    connection.createChannel((error1, channel) => {
       if (error1) {
         throw error1;
       }
@@ -15,9 +15,10 @@ async function sendMessage(queue, msg) {
       });
       channel.sendToQueue(queue, Buffer.from(msg));
 
+      // eslint-disable-next-line no-console
       console.log(' [x] Sent %s', msg);
     });
-    setTimeout(function () {
+    setTimeout(() => {
       connection.close();
       process.exit(0);
     }, 500);
