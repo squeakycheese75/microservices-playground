65687f1038e2851d6a98e66a81360069f96bc3ab
Niall Grant <niall.grant@and.digital>
2020-05-01 14:51:09 +0100

    Update amqp hostname

--- app2/receive.js
+++ app2/receive.js
@@ -1,6 +1,6 @@
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function (error0, connection) {
+amqp.connect('amqp://rabbitmq', function (error0, connection) {
   if (error0) {
     throw error0;
   }
